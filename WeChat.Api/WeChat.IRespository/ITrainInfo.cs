ec9516dc05da9b5a6aff2f3382ee6ac1d45f362b
杰哥 <46183272+wjjiege@users.noreply.github.com>
2019-01-30 15:29:33 +0800

    wangjie

---
+++ WeChat.Api/WeChat.IRespository/ITrainInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeChat.Model;
+
+namespace WeChat.IRespository
+{
+    /// <summary>
+    /// 火车信息接口
+    /// </summary>
+    public interface ITrainInfo
+    {
+        List<TrainTicketInfo> ShowTrainInfo();
+
+
+
+
+
+    }
+}
---
+++ WeChat.Api/WeChat.Respository/TrainInfo.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeChat.IRespository;
+using WeChat.Model;
+
+namespace WeChat.Respository
+{
+    /// <summary>
+    /// 火车票信息类
+    /// </summary>
+    public class TrainInfo : ITrainInfo
+    {
+        /// <summary>
+        /// 获取所有火车票信息
+        /// </summary>
+        /// <returns></returns>
+        public List<TrainTicketInfo> ShowTrainInfo()
+        {
+            string connStr = "Data Source=Jack;Database=wechat;User ID=root;Pwd=199901";
+            using (IDbConnection con = new MySqlConnection(connStr))
+            {
+                string str = "select * from TrainTicketInfo";
+                return con.Query<TrainTicketInfo>(str).ToList();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
