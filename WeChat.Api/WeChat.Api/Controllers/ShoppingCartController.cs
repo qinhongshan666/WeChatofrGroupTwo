4a9b3b229d774a9cb9b5ffbcf1fb175e2bd7c35d
tiansiqi <46983326+tiansiqi@users.noreply.github.com>
2019-02-12 14:31:56 +0800

    tiasniqi

--- WeChat.Api/WeChat.Api/Controllers/ShoppingCartController.cs
+++ WeChat.Api/WeChat.Api/Controllers/ShoppingCartController.cs
@@ -1,23 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-
-using WeChat.Model;
-using WeChat.IRespository;
-using WeChat.Respository;
-
-namespace WeChat.Api.Controllers
+﻿namespace WeChat.Api.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    using WeChat.IRespository;
+    using WeChat.Model;
+    using WeChat.Respository;
+
     public class ShoppingCartController : ApiController
     {
         public IBusTicketRepository BusTicketRepository { get; set; }
+
         [HttpGet]
         public List<BusIndent> BusIndents()
-        {         
-           var busIndents = BusTicketRepository.BusIndents();
+        {
+           var busIndents = this.BusTicketRepository.BusIndents();
             return busIndents;
         }
     }
--- WeChat.Api/WeChat.Api/Global.asax.cs
+++ WeChat.Api/WeChat.Api/Global.asax.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-using WebApplicationAutofac;
-
-namespace WeChat.Api
+﻿namespace WeChat.Api
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Http;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+    using WebApplicationAutofac;
+
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
