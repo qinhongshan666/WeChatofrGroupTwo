7fce95ef4643d93e9afdb094200c5dce4ac4c42f
qinhongshan <318921218@qq.com>
2019-02-13 21:34:43 +0800

    q
    去去去

--- WeChat.Api/WeChat.Common/CinfigHelper.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WeChat.Common
-{
-    public class CinfigHelper
-    {
-        // 获取配置文件中的内容
-    }
-}
--- WeChat.Api/WeChat.Respository/BusTicketRepository.cs
+++ WeChat.Api/WeChat.Respository/BusTicketRepository.cs
@@ -6,12 +6,14 @@ using System.Linq;
 
 using WeChat.IRespository;
 using WeChat.Model;
+using WeChat.Common;
 
 namespace WeChat.Respository
 {
     public class BusTicketRepository : IBusTicketRepository
     {
-        private string connStr = "Data Source=169.254.240.201;Database=wechat;User ID=root;Pwd=10086";
+        //private string connStr = "Data Source=169.254.240.201;Database=wechat;User ID=root;Pwd=10086";
+        private string connStr = ConfigHelper.GetConfigValue("sqlConnectionString");
 
         /// <summary>
         /// 获取 已经实现的汽车票订单
