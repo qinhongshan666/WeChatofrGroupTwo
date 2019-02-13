683309d914b70f08882705414a61a74d4525c7f6
杰哥 <46183272+wjjiege@users.noreply.github.com>
2019-02-13 08:31:29 +0800

    wj2.13

--- WeChat.Api/WeChat.Api/Controllers/TrainInfoController.cs
+++ WeChat.Api/WeChat.Api/Controllers/TrainInfoController.cs
@@ -7,8 +7,9 @@ using System.Web.Mvc;
 namespace WeChat.Api.Controllers
 {
     using Model;
+    using System.Web.Http;
     using WeChat.Respository;
-    public class TrainInfoController : Controller
+    public class TrainInfoController : ApiController
     {
         TrainInfo traininfo = new TrainInfo();
 
@@ -17,6 +18,7 @@ namespace WeChat.Api.Controllers
         /// </summary>
         /// <param name="m"></param>
         /// <returns></returns>
+        [HttpPost]
         public int TrainOrderInfo(TrainTicketOrders m)
         {
             int i = traininfo.TrainOrderInfo(m);
@@ -24,10 +26,28 @@ namespace WeChat.Api.Controllers
         }
 
         /// <summary>
+        /// 根据条件查票
+        /// </summary>
+        /// <param name="BeginSite">始发地</param>
+        /// <param name="ArriveSite">终止站</param>
+        /// <param name="BeginTime">出发日期</param>
+        /// <returns></returns>
+        [HttpGet]
+        public List<TrainTicketInfo> GetTrainInfo(string BeginSite, string ArriveSite,string BeginTime)
+        {
+            List<TrainTicketInfo> trainInfos = traininfo.ShowTrainInfo().ToList();
+
+            trainInfos = trainInfos.Where(m => !string.IsNullOrEmpty(BeginSite) ? m.BeginSite.Equals(BeginSite) : true).Where(m => !string.IsNullOrEmpty(ArriveSite) ? m.ArriveSite.Equals(ArriveSite) : true).Where(m => !string.IsNullOrEmpty(BeginTime) ? m.BeginTime.Equals(BeginTime) : true).ToList();
+            return trainInfos;
+        }
+
+
+        /// <summary>
         /// 根据ID查询火车票 然后跳转到支付页面
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
+        [HttpGet]
         public TrainTicketInfo Find(int ID)
         {
             return traininfo.Find(ID);
