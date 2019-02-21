d85543bb4636869936adb281d9ed08f5e9229e0c
刘庆洋 <1213774747@qq.com>
2019-02-21 14:10:16 +0800

    123

--- WchatTwo/pages/Bus_details/Bus_details.js
+++ WchatTwo/pages/Bus_details/Bus_details.js
@@ -1,9 +1,7 @@
 // pages/Bus_details/Bus_details.js
 const app = getApp();
 Page({
-
   data: {
-
     ticket: 1,
     busPrice: '',
     startingStation: '',
@@ -16,22 +14,18 @@ Page({
     phone:'',
     idnumber:'',
   },
-
   onLoad: function (options) {
     var id = options.busid;
     var that = this;
-
     wx.request({
       url: 'http://localhost:61984/api/Bus/GetBus',
       method: 'GET',
       data: {
         id: id
       },
-
       success(res) {
         var list = res.data;
         that.setData({
-
           busPrice: list.BusPrice,
           startingStation: list.StartingStation,
           destinationStation: list.DestinationStation,
@@ -39,91 +33,8 @@ Page({
           startTime: list.StartTime,
           endTime: list.EndTime,
           count: list.Count,
-          
         })
       }
-    })
-  },
-
-  toPay: function () {
-    var state=0;
-    var that = this.data;
-    wx.request({
-      url: 'http://localhost:61984/api/Bus/addbuss',
-      method: 'POST',
-      data: {
-        BusPrice: that.busPrice,
-        StartingStation: that.startingStation,
-        DestinationStation: that.destinationStation,
-        StartDate: that.startDate,
-        StartTime: that.startTime,
-        EndTime: that.endTime,
-        Count: that.count,
-        OrderState:state,
-        Name:that.name,
-        Phone:that.phone,
-        IDnumber:that.idnumber,
-      },
-      success(res) {
-        var i = res.data;
-        if (i == 1) {
-          wx.navigateTo({
-            url: '../checkBus/checkBus',
-          })
-        }
-
-      }
-    })
-  },
-
-    wx.getStorage({
-      key: 'token',
-      success: function (res) {
-        console.log(res.data)
-        console.log(111111111111)
-        wx.request({
-          url: 'http://localhost:61984/api/Bus/addbuss',
-          method: 'POST',
-          data: {
-            BusPric: that.busPrice,
-            StartingStation: that.startingStation,
-            DestinationStation: that.destinationStation,
-            StartDate: that.startDate,
-            StartTime: that.startTime,
-            EndTime: that.endTime,
-            Count: that.count
-          },
-          header: {
-            'content-type': 'application/json',
-            'Authorization': 'BasicAuth ' + res.data
-          },
-          success(res) {
-            var i = res.data;
-            if (i == 1) {
-              wx.navigateTo({
-                url: '../checkBus/checkBus',
-              })
-            }
-
- toPays: function () {
-   var state = 1;
-    var that = this.data;
-      console.log(that.startDate)
-    wx.request({
-      url: 'http://localhost:61984/api/Bus/addbuss',
-      method: 'POST',
-      data: {
-        BusPrice: that.busPrice,
-        StartingStation: that.startingStation,
-        DestinationStation: that.destinationStation,
-        StartDate: that.startDate,
-        StartTime: that.startTime,
-        EndTime: that.endTime,
-        Count: that.count,
-        OrderState: state,
-          }
-        })
-      },
     })
   },
   ticPhone: function (e) {
@@ -131,14 +42,17 @@ Page({
       phone: e.detail.value,
     })
   },
+
   ticName: function (e) {
     this.setData({
       name: e.detail.value,
     })
   },
+
   ticidnumber: function (e) {
     this.setData({
       idnumber: e.detail.value,
     })
-  }
-})
+  },
+    
+    })
